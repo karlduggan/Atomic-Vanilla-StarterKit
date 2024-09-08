5c94c4a7b8cc3f6a2a83ebd09c2b079a32a0b0e1
Karl Duggan <52557899+karlduggan@users.noreply.github.com>
2024-09-08 20:22:01 +0100

    updated pages

--- src/scripts/faq_dropdown_script.js
+++ src/scripts/faq_dropdown_script.js
@@ -1,22 +1,31 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const faqButtons = document.querySelectorAll('button[aria-expanded]');
+  const faqButtons = document.querySelectorAll('button[aria-expanded]');
   
-    faqButtons.forEach(button => {
-      button.addEventListener('click', () => {
-        const expanded = button.getAttribute('aria-expanded') === 'true';
-        const answer = button.nextElementSibling;
-  
-        // Toggle visibility
-        if (expanded) {
-          answer.style.display = 'none'; // Hide the answer
-        } else {
-          answer.style.display = 'block'; // Show the answer
+  faqButtons.forEach(button => {
+    button.addEventListener('click', () => {
+      const expanded = button.getAttribute('aria-expanded') === 'true';
+      const answer = button.nextElementSibling;
+
+      // Collapse all other answers and remove classes
+      faqButtons.forEach(otherButton => {
+        if (otherButton !== button) {
+          otherButton.setAttribute('aria-expanded', 'false');
+          otherButton.classList.remove('bg-highlight', 'text-white');
+          otherButton.nextElementSibling.style.display = 'none';
         }
-  
-        // Toggle button state
-        button.setAttribute('aria-expanded', !expanded);
       });
+
+      // Toggle visibility for the clicked button
+      if (expanded) {
+        answer.style.display = 'none'; // Hide the answer
+        button.classList.remove('bg-highlight', 'text-white'); // Remove classes
+      } else {
+        answer.style.display = 'block'; // Show the answer
+        button.classList.add('bg-highlight', 'text-white'); // Add classes
+      }
+
+      // Toggle button state
+      button.setAttribute('aria-expanded', !expanded);
     });
   });
-  
-  
+});
